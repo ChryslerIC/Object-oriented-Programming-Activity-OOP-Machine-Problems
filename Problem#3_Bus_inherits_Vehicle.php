ae87b144d19c50cf2467fe2a096bb55e85e53bc3
ChryslerIC <154021471+ChryslerIC@users.noreply.github.com>
2024-10-17 05:04:26 +0800

    #3_Bus_inherits_Vehicle.php

--- Problem#3_Bus_inherits_Vehicle.php
+++ Problem#3_Bus_inherits_Vehicle.php
@@ -1,7 +1,21 @@
 <?php
-require_once 'Class_Vehicle.php';
+
+
+class Vehicle {
+    public $name = 0;
+    public $speed = 0;
+    public $mileage = 0;
+
+    function __construct($name,$speed,$mileage){
+        $this->name = $name;
+        $this->speed = $speed;
+        $this->mileage = $mileage;
+    }
+}
 
 class Bus extends Vehicle {
-    
+
 }
+
+
 ?>
